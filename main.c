bd986c1c44a59e0aadfc05c5c445550237cb57d4
Adrian Munoz perez <amunoz-p@c1r2s1.42madrid.com>
2020-08-26 20:10:27 +0200

    pwd works

--- main.c
+++ main.c
@@ -6,11 +6,20 @@
 /*   By: amunoz-p <amunoz-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/10 18:10:01 by amunoz-p          #+#    #+#             */
-/*   Updated: 2020/08/26 19:38:09 by amunoz-p         ###   ########.fr       */
+/*   Updated: 2020/08/26 20:07:52 by amunoz-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "minishell.h"
+
+static void	ft_pwd(void)
+{
+	printf("OOEFJWFOWHFOWF\n");
+	char *id;
+	char *print;
+	print = getcwd(id, 50);
+	printf("%s\n", print);
+}
 
 int main(int argc, char **argv, char **env)
 {
@@ -19,11 +28,12 @@ int main(int argc, char **argv, char **env)
 	while (1)
 	{
 		get_next_line(0, &line);
-		if (ft_memcmp("exit", line, ft_strlen("exit")) == 0)
+		if (ft_strncmp("exit", line, ft_strlen(line)) == 0)
 			return(0);
-		//printf("hola\n");
-		printf("%s\n", line);
+		else if (ft_strncmp("pwd", line, ft_strlen(line)) == 0)
+			ft_pwd();
 		
+		printf("$%s\n", line);
 	}
 	return 0;
 }
