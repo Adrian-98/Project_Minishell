ec0ae739fef65434c36d81231185d5680ef4a8d5
Gonzalo Lopez andres <glopez-a@c3r6s1.42madrid.com>
2020-09-15 20:23:16 +0200

    para ti adri

--- ft_bash.c
+++ ft_bash.c
@@ -6,7 +6,7 @@
 /*   By: glopez-a <glopez-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/29 11:33:02 by adrian            #+#    #+#             */
-/*   Updated: 2020/09/15 20:00:32 by glopez-a         ###   ########.fr       */
+/*   Updated: 2020/09/15 20:22:35 by glopez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@ void	ft_bash(t_shell *f)
 	int	j;
 
 	i = 0;
-	while (f->path[i])
-	{
-		aux = ft_strjoin(f->path[i], *f->arguments);
-		//printf("%s\n", aux);
-		j = execve(aux, f->arguments, f->envv);
-		free(aux);
-		i++;
-	}
-	printf("adios\n");
+	if (si es el proceso hijo)
+		while (f->path[i])
+		{
+			aux = ft_strjoin(f->path[i], *f->arguments);
+			//printf("%s\n", aux);
+			j = execve(aux, f->arguments, f->envv);
+			free(aux);
+			printf("%d\n", j);
+			i++;
+		}
+		ft_404(f);
 }
