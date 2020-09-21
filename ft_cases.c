e034df7213e14221dfa2f0673e5a64171647eeb1
Adrian Munoz perez <amunoz-p@c3r4s3.42madrid.com>
2020-09-21 18:05:09 +0200

    cd funciona

--- ft_cases.c
+++ ft_cases.c
@@ -6,7 +6,7 @@
 /*   By: amunoz-p <amunoz-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/29 11:33:02 by adrian            #+#    #+#             */
-/*   Updated: 2020/09/18 17:39:59 by amunoz-p         ###   ########.fr       */
+/*   Updated: 2020/09/21 18:01:10 by amunoz-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ void	ft_cases(t_shell *f)
 	else if (ft_strncmp("echo", f->arguments[0],
 			ft_strlen("echo")) == 0)
 		ft_echo(f);
-	// else if (ft_strncmp("cd", f->arguments[0],
-	// 		ft_strlen("cd")) == 0)
-	// 	ft_cd(f);
+	else if (ft_strncmp("cd", f->arguments[0],
+			ft_strlen("cd")) == 0)
+		ft_cd(f);
 	else
 		ft_bash(f);
 	f->line = 0;
