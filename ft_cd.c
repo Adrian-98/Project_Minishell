a3c404c5cc37bf8a16cc0cda493642a811bb8d96
Adrian Munoz perez <amunoz-p@c3r4s3.42madrid.com>
2020-09-21 18:45:36 +0200

    cd funciona con la raiz tmb

--- ft_cd.c
+++ ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: amunoz-p <amunoz-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/17 17:57:14 by amunoz-p          #+#    #+#             */
-/*   Updated: 2020/09/21 18:03:21 by amunoz-p         ###   ########.fr       */
+/*   Updated: 2020/09/21 18:45:07 by amunoz-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,11 @@ void		ft_cd(t_shell *f)
 		i++;
 	if (i >= 3)
 		ft_too_many(f);
+	else if ((f->arguments[1]) == 0)
+	{
+		if (chdir("/Users/amunoz-p/") != 0)
+			printf("%s\n", strerror(errno));
+	}
 	else if (chdir(f->arguments[1]) != 0)
 		printf("%s\n", strerror(errno));
 }
