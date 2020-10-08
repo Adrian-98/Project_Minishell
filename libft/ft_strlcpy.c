4178f735e95990b55f9f71c19434b7e48d9517e5
Gonzalo Lopez andres <glopez-a@c3r7s1.42madrid.com>
2020-10-08 21:08:05 +0200

    merge adria

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,13 +6,35 @@
 /*   By: glopez-a <glopez-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/06 18:46:18 by amunoz-p          #+#    #+#             */
-/*   Updated: 2020/09/15 18:18:01 by glopez-a         ###   ########.fr       */
+/*   Updated: 2020/10/08 20:59:31 by glopez-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t			i;
+	size_t			count;
+
+	count = 0;
+	i = 0;
+	if (!dst)
+		return (dstsize);
+	count = ft_strlen((char *)src);
+	if (dstsize > 0)
+	{
+		while (src[i] != '\0' && i < (dstsize - 1))
+		{
+			dst[i] = ((char*)src)[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	return (count);
+}
+
+size_t		ft_strlcpy2(char *dst, const char *src, size_t dstsize)
 {
 	size_t			i;
 	size_t			count;
